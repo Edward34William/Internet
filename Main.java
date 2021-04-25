e5c039e88dbb0bcb548597d7710c5305edbe47e8
Edward William <>
2021-04-25 01:55:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+
+    // camelCase
+    String internetSubeButonu = "Internet Subesi";
+
+    System.out.println(internetSubeButonu);
+  }
+}
